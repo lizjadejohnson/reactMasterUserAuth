5ccd61b7d6d41277d082de866f55d296327b55a1
Liz Johnson <liz.jade.johnson@gmail.com>
2024-06-05 15:43:40 -0400

    changed cookie settings

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -112,7 +112,8 @@ const createUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Use 'Lax' for local development
+            sameSite: 'None',
+            // sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Use 'Lax' for local development
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         });
         
@@ -184,7 +185,8 @@ const updateUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Use 'Lax' for local development
+            sameSite: 'None',
+            // sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Use 'Lax' for local development
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         });
 
@@ -254,7 +256,8 @@ const loginUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Use 'Lax' for local development
+            sameSite: 'None',
+            // sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Use 'Lax' for local development
             maxAge: 24 * 60 * 60 * 1000
         });
         //Return entire user data except the password:
