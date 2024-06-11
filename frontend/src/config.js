bd69640886e40e0eb3651c1b3797d861cbd865b5
Liz Johnson <liz.jade.johnson@gmail.com>
2024-06-11 07:49:04 -0400

    changing env urls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,19 +2,20 @@
 //In production, apiUrl will be https://react-master-template-backend.onrender.com/api (because we'll have that set in Render)
 
 
-// let apiUrl = '';
-
-// if (process.env.NODE_ENV === 'development') {
-//   apiUrl = 'http://localhost:3000/api';
-// } else if (process.env.NODE_ENV === 'production') {
-//   // apiUrl = 'https://react-master-template.onrender.com/api';
-//   apiUrl = 'https://react-auth-template.onrender.com';
-// }
-
-
-let apiUrl = '/api';
-
-console.log('API URL:', apiUrl);  // This should output the correct API URL based on the environment
-console.log('Current Environment:', process.env.NODE_ENV);  // This should output the current environment setting
+let apiUrl = '';
+if (process.env.NODE_ENV === 'development') {
+  apiUrl = 'http://localhost:3000/api';
+} else if (process.env.NODE_ENV === 'production') {
+  // apiUrl = 'https://react-master-template.onrender.com/api';
+  apiUrl = 'https://react-auth-template.onrender.com';
+}
 export default apiUrl;
+
+
+// let apiUrl = '/api';
+
+// console.log('API URL:', apiUrl);  // This should output the correct API URL based on the environment
+// console.log('Current Environment:', process.env.NODE_ENV);  // This should output the current environment setting
+
+// export default apiUrl;
 
